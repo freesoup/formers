95b4e68b4d84e615a721ebe7f7d6798fed1625e8
Jackie Tan Jin Wei <wei_ja@worksap.co.jp>
2018-07-23 16:07:04 +0800

    define properties of an input

--- src/formers/core/object/Input.java
+++ src/formers/core/object/Input.java
@@ -1,5 +1,24 @@
 package formers.core.object;
 
 public class Input {
+    private String question;
+    // TODO: Change to enum
+    private String type;
+    private String paramName;
+    private String placeholder;
+    private boolean checked;
 
+    public Input(String question, String type, String name) {
+        this.question = question;
+        this.type = type;
+        this.paramName = name;
+    }
+
+    public void setPlaceholder(String info) {
+        this.placeholder = info;
+    }
+
+    public void setChecked(boolean enabled) {
+        this.checked = enabled;
+    }
 }

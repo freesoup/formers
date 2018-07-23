41487c1b111bb1fb22c807fdb54f5962bc113949
Jackie Tan Jin Wei <wei_ja@worksap.co.jp>
2018-07-23 16:29:51 +0800

    add createForm Logic

---
+++ src/formers/core/object/FormID.java
@@ -0,0 +1,7 @@
+package formers.core.object;
+
+public class FormID {
+    public static String generateFormID() {
+        return "abc123";
+    }
+}
--- src/formers/core/users/AdminCore.java
+++ src/formers/core/users/AdminCore.java
@@ -1,7 +1,30 @@
 package formers.core.users;
 
+import java.util.List;
+
+import formers.core.object.Form;
+import formers.core.object.FormID;
+import formers.core.object.Input;
+
 public class AdminCore {
-    public void createForm() {
+    String accountName;
+
+    public Form initForm() {
+        Form form = new Form();
+        form.addAdmin(accountName);
+        form.addID(FormID.generateFormID());
+
+        return form;
+    }
+
+    public void addField(Form form, List<Input> inputList) {
+        for (Input field : inputList) {
+            form.addInput(field);
+        }
+    }
+
+    public void viewResult(String ID) {
 
     }
+
 }
